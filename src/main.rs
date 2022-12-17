7b6e122297c10d295c9cd80ec436a720ac6490bf
Clement Calmels <clement.calmels@free.fr>
2022-12-17 14:39:59 +0100

    update CLI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use clap::Parser;
 mod days;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(trailing_var_arg = true)]
+    days: Vec<u32>,
+}
+
 fn main() {
-    advent_2022::resolve_all();
+    let args = Args::parse();
+
+    if args.days.is_empty() {
+        advent_2022::resolve_all();
+    } else {
+        for d in args.days {
+            advent_2022::resolve_one(d);
+        }
+    }
 }
