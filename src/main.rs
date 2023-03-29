6d56efc82ff18a28e2c761ca7555c8d0e22f2efa
Clement Calmels <clement.calmels@free.fr>
2023-03-29 18:36:23 +0200

    limit the number of threads for rayon

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,21 @@ mod days;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[arg(short, long, help = "limit the number of threads used by rayon")]
+    nthreads: Option<usize>,
     #[arg(trailing_var_arg = true)]
     days: Vec<u32>,
 }
 
 fn main() {
     let args = Args::parse();
+
+    if let Some(nthreads) = args.nthreads {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(nthreads)
+            .build_global()
+            .unwrap();
+    }
 
     if args.days.is_empty() {
         advent_2022::resolve_all();
